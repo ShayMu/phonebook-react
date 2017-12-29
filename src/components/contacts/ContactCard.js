b64770ae82784523dfb87a914bcfddc895fca094
Shay Mualem <Shaymu01@gmail.com>
2017-12-29 20:37:14 +0200

    Add contact card styling

--- src/components/contacts/ContactCard.js
+++ src/components/contacts/ContactCard.js
@@ -2,9 +2,15 @@ import React, { PropTypes } from 'react';
 
 const ContactCard = ({ fname, lname, phone }) => {
   return (
-    <div className="row">
-      <div className="col-3">{fname + " " + lname}</div>
-      <div className="col-3">{phone}</div>
+    <div className="contactCard">
+      <div><i className="fa fa-user-secret" style={{ fontSize: "4em", paddingTop: "0.2em" }}></i></div>
+      <div style={{ marginTop: "0.8em", marginBottom: "0.8em", lineHeight: "1.5em", height: "3em", overflow: "hidden" }}>{fname + " " + lname}</div>
+      <div><i className="fas fa fa-phone"></i> {phone}</div>
+
+      <div className="editOverlay">
+        <i className="fas fa-pencil-alt"></i>
+        <i className="fas fa-trash-alt"></i>
+      </div>
     </div>
   );
 };

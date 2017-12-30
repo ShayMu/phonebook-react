c291ea52655e9ecabdc0ce8a34cf8396037802d8
Shay Mualem <Shaymu01@gmail.com>
2017-12-30 18:33:31 +0200

    Finished the contacts reducer logic

--- src/reducers/contactsReducer.js
+++ src/reducers/contactsReducer.js
@@ -3,8 +3,8 @@ import initialState from './initialState';
 
 export default function contactsReducer(state = initialState.contacts, action) {
   switch (action.type) {
-    case types.ADD_CONTACT: return state;
-    case types.EDIT_CONTACT: return state;
+    case types.ADD_CONTACT: return [...state, Object.assign({}, action.info)];
+    case types.EDIT_CONTACT: return [...state.map((item) => { if (item.key === action.info.key) { return Object.assign({}, action.info); } return item; })];
     case types.REMOVE_CONTACT: return [...state.filter(item => action.info.key !== item.key)];
 
     default: return state;
